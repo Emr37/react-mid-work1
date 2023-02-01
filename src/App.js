a1826bf8b1cf691283f1bf4851175876ec3c2b42
Emrah Aksoy <emraksoy37@gmail.com>
2023-02-02 02:47:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  const [userId, setUserId] = useState("");
+  
+  const getData = async(_userId) => {
+    const { data: users} = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const { data: posts} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+
+    console.log(users);
+    console.log(posts);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      Hello World
+      <br/>
+      <input onChange={(e) => setUserId(e.target.value)} placeholder="Please write user id"></input>
+      <br/>
+      <button onClick={getData}>Get Data</button>
+      
     </div>
   );
 }
